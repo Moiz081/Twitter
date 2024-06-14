87465ffb209e5b574506f8e0657d4740c8570190
Md Moizuddin <moizmd066@gmail.com>
2024-06-14 11:28:49 +0530

    Added comments api

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -1,13 +1,23 @@
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
 const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
-        max: ['250', 'Tweet cannot be more than 250 characters']
-    }
-}, { timestamps: true });
+        max: [250, 'Tweet cannot be more than 250 characters']
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Like'
+        }
+    ],
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+}, {timestamps: true});
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
-
 export default Tweet;
