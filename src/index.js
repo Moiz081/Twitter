1d80207acd04b6a028cc0704c6882f1336ff47dc
Md Moizuddin <moizmd066@gmail.com>
2024-06-14 11:03:49 +0530

    Added routes and controllers for create tweet api

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { connect } from './config/db.js';
 
-// import HashtagRepository from './repository/hashtag-repository.js';
-import TweetService from './services/tweet-services.js';
+import apiRoutes from './routes/index.js';
 
 const setUpAndStartServer = async () => {
 
@@ -12,36 +11,11 @@ const setUpAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
+    app.use('/api', apiRoutes);
+
     app.listen(3000, async () => {
         console.log(`Server started at PORT:${3000}`);
         await connect();
-
-        // const hashtags = new HashtagRepository();
-        // const response = await hashtags.bulkCreate(
-        //     {
-        //         title: '#excites',
-        //         tweets: []
-        //     },
-        //     {
-        //         title: '#bore',
-        //         tweets: []
-        //     },
-        //     {
-        //         title: '#coding',
-        //         tweets: []
-        //     },
-        //     {
-        //         title: '#fun',
-        //         tweets: []
-        //     }
-        // )
-        
-        const tweet = new TweetService();
-        const response = await tweet.create({
-            content: 'This is my #first tweet, #excited #nice day'
-        })
-        console.log(response);
-
     });
 }
 
