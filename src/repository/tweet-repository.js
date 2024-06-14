2a876e4e874ceed383b23b74615cc87312d24ded
Md Moizuddin <moizmd066@gmail.com>
2024-06-14 11:58:12 +0530

    Added image upload in tweet api

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,6 +1,10 @@
 import Tweet from '../models/tweet.js'
+import CrudRepository from './crud-repository.js';
 
-class TweetRepository {
+class TweetRepository extends CrudRepository {
+    constructor() {
+        super(Tweet);
+    }
     
     async create(data) {
         try {
@@ -11,27 +15,14 @@ class TweetRepository {
         }
     }
 
-    async get(id) {
-        try {
-            const tweet = await Tweet.findById(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     async getWithComments(id) {
         try {
-            const tweet = await Tweet.findById(id).populate({path: 'comments'}).lean();
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async destroy(id) {
-        try {
-            const tweet = await Tweet.findByIdAndRemove(id);
+            const tweet = await Tweet.findById(id).populate({
+                path: 'comments',
+                populate: {
+                    path: 'comments'
+                }
+            }).lean();
             return tweet;
         } catch (error) {
             console.log(error);
@@ -41,6 +32,15 @@ class TweetRepository {
     async getAll(offset, limit) {
         try {
             const tweet = await Tweet.find().skip(offset).limit(limit);
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async find(id) {
+        try {
+            const tweet = await Tweet.findById(id).populate({path: 'likes'});
             return tweet;
         } catch (error) {
             console.log(error);
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,19 +1,25 @@
+
 import express from 'express';
 
 import { createTweet, getTweet } from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
 import { createComment } from '../../controllers/comment-controller.js';
+import { signup, login } from '../../controllers/auth-controller.js';
+
+import { authenticate } from '../../middlewares/authenticate.js'
 
 const router = express.Router();
 
-router.post('/tweets', createTweet);
+router.post('/tweets', authenticate, createTweet);
 router.get('/tweets/:id', getTweet);
 
 router.post('/likes/toggle',toggleLike);
 
 
-router.post('/comments', createComment);
+router.post('/comments', authenticate, createComment);
 
 router.post('/signup', signup);
+
+router.post('/login', login);
 
 export default router;
